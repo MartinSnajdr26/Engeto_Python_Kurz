67d5595fb3163d88db6675910a693bb9086b0bc9
MartinSnajdr26 <martin.snajdr.japan@gmail.com>
2024-12-04 20:11:47 +0100

    lesson 2

---
+++ Engeto_Python_course/python_intro.py
@@ -0,0 +1,15 @@
+hockey = {
+    "league": "NHL",
+    "Teams": 32,
+    "Players": [
+        {"name": "Rudy Gay", "position": "Center", "goals": 38},
+        {"name": "Lewis Carroll", "position": "Left Wing", "goals": 29},
+        {"name": "Matt Gasparro", "position": "Defense", "goals": 22},
+        {"name": "Dave Carroll", "position": "Goalie", "goals": 0}  
+    ]
+}
+
+print(hockey["Players"])
+
+
+
